b4457f9013694783f23a978804cf57814f20d106
bmostowski1996 <bmostowski1996@gmail.com>
2025-03-25 19:15:04 -0400

    some chatroom routes, debugging is needed, however

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,6 +1,11 @@
 import sequelize from '../config/connection.js'
+import { ChatroomFactory } from './chatroom.js';
 import { UserFactory } from './user.js';
 
 const User = UserFactory(sequelize);
+const Chatroom = ChatroomFactory(sequelize);
 
-export { User };
+Chatroom.belongsTo(User, { foreignKey: 'owner', onDelete: 'SET NULL' });
+User.hasMany(Chatroom, { foreignKey: 'owner' });
+
+export { User, Chatroom };
---
+++ server/src/routes/api/chatroom-routes.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { Chatroom } from '../../models/index.js';
+
+
+const router = express.Router();
+
+// GET /chatrooms - Get all chatrooms
+router.get('/', async (_req: Request, res: Response) => {
+  try {
+    const chatrooms = await Chatroom.findAll();
+    res.json(chatrooms);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// /POST /chatrooms/create - Create a new chatroom
+router.post('/create', async (req: Request, res: Response) => {
+    if (!req.user) {
+        return res.status(401);
+    }
+    
+    try {
+        const name = req.body.name;
+        const owner = req.user.id;
+        const newChatroom = await Chatroom.create({ name, owner });
+        return res.status(201).json(newChatroom);
+    } catch (error: any) {
+        return res.status(500).json({ message: error.message });
+    }
+});
+
+export { router as chatroomRouter };
