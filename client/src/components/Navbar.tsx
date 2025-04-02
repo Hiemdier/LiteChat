29b8cd3c35e1f8dce263daddf686a502a3f495e2
bmostowski1996 <bmostowski1996@gmail.com>
2025-04-01 22:24:34 -0400

    added some styling, very cool design evan

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -19,8 +19,8 @@ const Navbar = () => {
   }, [loginCheck]);  // Dependency array ensures useEffect runs when loginCheck changes
 
   return (
-    <div className="display-flex justify-space-between align-center py-2 px-5 mint-green">
-      <h1>
+    <div className="display-flex justify-space-between align-center py-2 px-5 mint-green lc-navbar">
+      <h1 style={{color: 'white'}}>
         LiteChat
       </h1>
       <div>
--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
 import Auth from '../utils/auth';
 
-const webAddress = "https://litechat-cebn.onrender.com/";
-
-const socket = io( webAddress || "http://localhost:3001", { 
+const socket = io("http://localhost:3001", { 
   withCredentials: true, // Ensure credentials are passed correctly
   transports: ["websocket"],
   auth: {
